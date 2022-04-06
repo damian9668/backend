e2510026a600123f2b494eb8425dd3af2022d8ce
Damian <damian.ullmann@gmail.com>
2022-04-06 12:53:46 -0300

    desafio4 fix

--- public/index.js
+++ public/index.js
@@ -3,4 +3,30 @@ function search(){
     const id = document.getElementById("id").value;
     window.location.replace("/api/productos/"+id);
 }
+function eliminar(){
+    const id = document.getElementById("id2").value;
+    fetch('http://localhost:8080/api/productos/'+id, {
+        method: "DELETE",
+        headers: {
+            'Content-type': 'application/json'
+        }
+    })
+}
+function update(){
+    const id = document.getElementById("id3").value;
+    const titulo = document.getElementById("titulo").value;
+    const precio = document.getElementById("precio").value;
+    const urlImag = document.getElementById("urlImag").value;
 
+    fetch('http://localhost:8080/api/productos/'+id, {
+        method: "PUT",
+        headers: {
+            'Content-type': 'application/json'
+        },
+        body: JSON.stringify(
+            { title: titulo,
+                    price: precio,
+                    thumbnail:urlImag}
+        )
+    })
+}
