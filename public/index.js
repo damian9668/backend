25bf14a13969ea47f5b79d70224dea8bd593b1a7
Damian <damian.ullmann@gmail.com>
2022-04-06 11:11:27 -0300

    desafio 4

--- public/index.js
+++ public/index.js
@@ -3,10 +3,4 @@ function search(){
     const id = document.getElementById("id").value;
     window.location.replace("/api/productos/"+id);
 }
-async function enviar(){
-    const id = document.getElementById("id").value
 
-    fetch('/api/productos/' + id, {
-        method: 'PUT',
-    })
-}
