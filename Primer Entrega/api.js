eef3bcdb0c58ee107693f4615b551eb4fa71489b
Damian <damian.ullmann@gmail.com>
2022-04-29 11:50:36 -0300

    Reentrega

--- Primer Entrega/api.js
+++ Primer Entrega/api.js
@@ -3,6 +3,7 @@ const contenedor = new Contenedor('productos.txt');
 const contenedorCarrito = new Contenedor('carritos.txt');
 
 const express = require("express");
+const {response} = require("express");
 const { Router } = express;
 
 const app = express();
@@ -44,8 +45,9 @@ router.get("/:id?",async (req, res)=>{
         res.json(message);
     }else{
         const resp = await contenedor.getById(id)
-        if(resp.error != null){
-            res.status(400).send(resp);
+       // console.log(resp)
+        if(resp == undefined){
+            res.status(400).send({error:"Producto No Encontrado"});
         }
         else{
             res.json(resp);
@@ -76,10 +78,10 @@ router.put("/:id",soloAdmins,async(req, res)=>{
 router.delete("/:id",soloAdmins,async (req, res)=>{
     const id = req.params.id;
     const message = await contenedor.deleteById(id);
-    console.log(message);
+    //console.log(message.error)
 
     if(message.error != null){
-        res.status(400).send(message);
+        res.status(400).send({error:"producto no encontrado"});
     }
     else{
         res.json(message);
@@ -97,14 +99,20 @@ routerCarrito.post("/:id/productos",async (req, res)=>{
    const id = req.params.id;
    const producto = req.body
    const resp = await contenedorCarrito.updateCarritoById(id,producto);
-   res.json(resp);
+
+    if(resp.error != null){
+        res.status(400).send("Carrito No Encontrado");
+    }
+    else{
+        res.json(resp);
+    }
 })
 
 routerCarrito.get("/:id/productos",async (req, res)=>{
     const id = req.params.id;
     const resp = await contenedorCarrito.getById(id)
-    if(resp.error != null){
-        res.status(400).send(resp);
+    if(resp == undefined){
+        res.status(400).send("Carrito No Encontrado");
     }
     else{
         res.json(resp.productos);
@@ -115,16 +123,15 @@ routerCarrito.delete("/:id/productos/:id_prod",async (req, res)=>{
     const idCarrito = req.params.id;
     const idProducto = req.params.id_prod;
     const carrito = await contenedorCarrito.getById(idCarrito);
-    if(carrito.error != null){
-        res.status(400).send(carrito);
+    if(carrito == undefined){
+        res.status(400).send("Carrito No Encontrado");
     }
     else{
-        if(carrito.productos.id == idProducto){
-            const resp = await contenedorCarrito.updateCarritoById(idCarrito,"");
-            res.json(resp);
-        }
-        else{
-            res.status(400).send("producto no encontrado");
+       const response = await contenedorCarrito.deleteProdFromCarritoById(carrito,idProducto)
+        if(response.error != null){
+            res.status(400).send(response);
+        }else{
+            res.json(response)
         }
 
     }
@@ -133,10 +140,10 @@ routerCarrito.delete("/:id/productos/:id_prod",async (req, res)=>{
 routerCarrito.delete("/:id",async (req, res)=>{
     const id = req.params.id;
     const message = await contenedorCarrito.deleteById(id);
-    //console.log(message);
+    //console.log(message.error);
 
     if(message.error != null){
-        res.status(400).send(message);
+        res.status(400).send({error:"Carrito No Encontrado"});
     }
     else{
         res.json("carrito eliminado");
--- Primer Entrega/funciones.js
+++ Primer Entrega/funciones.js
@@ -6,6 +6,7 @@ class Contenedor{
     constructor(nombre){
         this.archivo=nombre;
     }
+    //no tocar
     async save(object){
         try{
             const contenido = await fs.promises.readFile(this.archivo,'utf8');
@@ -38,27 +39,19 @@ class Contenedor{
             console.log(error);
         }
     }
-
+    //no tocar
     async getById(id){
         try{
             const contenido = await fs.promises.readFile(this.archivo,'utf8');
             const info = JSON.parse(contenido);
-            let i=0;
-            if (id > info.length){
-                throw new Error("Ups");
-            }
-            for(i=0;i<info.length;i++){
-                if(info[i].id == id){
-                   // console.log(info[i]);
-                    return info[i];
-                }
-            }
+            const response = info.find(element => element.id == id)
+            return response;
         }catch(error){
             console.log(error);
-            return {error:"producto no encontrado"};
+            return {error:"no encontrado"};
         }
     }
-
+    //no tocar
     async getAll(){
         try{
             const contenido = await fs.promises.readFile(this.archivo,'utf8');
@@ -70,24 +63,23 @@ class Contenedor{
             console.log(error);
         }
     }
-
+    //no tocar
     async deleteById(id){
         try{
             const contenido = await fs.promises.readFile(this.archivo,'utf8');
             const info = JSON.parse(contenido);
+            const index = info.findIndex(prod =>prod.id == id);
 
-            const result =  info.filter(objetos => objetos.id != id);
-            await fs.promises.writeFile(this.archivo,JSON.stringify(result,null,2));
-            console.log(result);
-
-            if (id > info.length){
-                throw new Error("Ups");
+            if(index == -1){
+                throw new Error("No Encontrado")
+            }else{
+                const result =  info.filter(objetos => objetos.id != id);
+                await fs.promises.writeFile(this.archivo,JSON.stringify(result,null,2));
+                return result;
             }
-            return result;
-
         }catch(error){
             console.log(error);
-            return {error:"producto no encontrado"};
+            return {error:error};
         }
     }
 
@@ -104,22 +96,25 @@ class Contenedor{
             console.log(error);
         }
     }
+    //no tocar
     async updateById(id,updateProd){
         try{
             const contenido = await fs.promises.readFile(this.archivo,'utf8');
             const info = JSON.parse(contenido);
-            if (id > info.length){
-                throw new Error("Ups");
-            }
-            console.log(info);
+           // console.log(info);
             const index = info.findIndex(prod =>prod.id == id);
-            updateProd.id=id;
-            info[index]=updateProd;
-            console.log(index);
-            await fs.promises.writeFile(this.archivo,JSON.stringify(info,null,2));
-            console.log(updateProd);
-            return "OK";
 
+            if(index == -1){
+                throw new Error("Producto No Encontrado")
+            }else{
+                updateProd.id=id;
+                updateProd.timestamp=Date.now()
+                info[index]=updateProd;
+                console.log(index);
+                await fs.promises.writeFile(this.archivo,JSON.stringify(info,null,2));
+                //console.log(updateProd);
+                return "OK";
+            }
         }catch(error){
             console.log(error);
             return {error:"producto no encontrado"};
@@ -141,6 +136,30 @@ class Contenedor{
             console.log(error);
             return {error:"producto no encontrado"};
         }
+    }
+    async deleteProdFromCarritoById(carrito,idProducto){
+        try{
+            const indexProd = carrito.productos.findIndex(prod =>prod.id == idProducto)
+
+            if(indexProd == -1){
+                throw new Error("Producto No Encontrado")
+            }else{
+                carrito.productos.splice(indexProd,1)
+                const contenido = await fs.promises.readFile(this.archivo,'utf8');
+                const info = JSON.parse(contenido);
+                const index = info.findIndex(carritos =>carritos.id == carrito.id)
+                info[index].productos = carrito.productos
+                info[index].timestamp = Date.now();
+                await fs.promises.writeFile(this.archivo,JSON.stringify(info,null,2));
+                return "OK";
+            }
+        }catch (e){
+            console.log(e);
+            return {error:"producto no encontrado"};
+        }
+
+
+
     }
 }
 
